f5b2e22c72d4f5447713bafb5c751a064ee1670a
Saran <saranbabu2609@gmail.com>
2022-12-25 11:12:36 +0530

    second commit

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
  db = async()=>{
    try {
-      await mongoose.connect(process.env.MONGO_URL);
+      await mongoose.connect("mongodb+srv://sarans:fullstackdemobackend@full-stack-demo-backend.drq0dgt.mongodb.net/employees?retryWrites=true&w=majority");
         console.log('Db Connnection Established')
     } catch(error){
         console.log('DB Error: ',error);
